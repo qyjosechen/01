37b5fa2c0143fd3dd6f4a1373589549511c42ded
qyjosechen <117866668+qyjosechen@users.noreply.github.com>
2022-11-09 19:27:38 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ +-------------------------------------------------------------------------+
+ | Author: Jose Chen <jose@airli.cn>                                       |
+ | Author: Airli Tian <im@airli.cn>                                        |
+ +-------------------------------------------------------------------------+
+*/
+define( '01_UT', true );
+
+/** Loads the 01 Environment and Template */
+require __DIR__ . '/01_heard.php';
